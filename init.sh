235d7859d85b0c6c7d8a7a8dcb71be4feb3826bd
Daniel Scholl <dascholl@microsoft.com>
2020-01-06 09:13:41 -0600

    Removed Service Principal as not needed.

--- init.sh
+++ init.sh
@@ -42,47 +42,6 @@ PRINCIPAL_NAME=${BASE}-${UNIQUE}
 ## FUNCTIONS                 ##
 ###############################
 
-function CreateServicePrincipal() {
-    # Required Argument $1 = PRINCIPAL_NAME
-
-    local _result=$(az ad sp list --display-name $1 --query [].appId -otsv)
-    if [ "$_result"  == "" ]
-    then
-      CLIENT_SECRET=$(az ad sp create-for-rbac \
-        --name $PRINCIPAL_NAME \
-        --skip-assignment \
-        --query password -otsv)
-      CLIENT_ID=$(az ad sp list \
-        --display-name $PRINCIPAL_NAME \
-        --query [].appId -otsv)
-      OBJECT_ID=$(az ad sp list \
-        --display-name $PRINCIPAL_NAME \
-        --query [].objectId -otsv)
-
-      echo "" >> .envrc
-      echo "export CLIENT_ID=${CLIENT_ID}" >> .envrc
-      echo "export CLIENT_SECRET=${CLIENT_SECRET}" >> .envrc
-      echo "export OBJECT_ID=${OBJECT_ID}" >> .envrc
-      echo "export UNIQUE=${UNIQUE}" >> .envrc
-    else
-        tput setaf 3;  echo "Service Principal $1 already exists."; tput sgr0
-        if [ -z $CLIENT_ID ]; then
-          tput setaf 1; echo 'ERROR: Principal exists but CLIENT_ID not provided' ; tput sgr0
-          exit 1;
-        fi
-
-        if [ -z $CLIENT_SECRET ]; then
-          tput setaf 1; echo 'ERROR: Principal exists but CLIENT_SECRET not provided' ; tput sgr0
-          exit 1;
-        fi
-
-        if [ -z $OBJECT_ID ]; then
-          tput setaf 1; echo 'ERROR: Principal exists but OBJECT_ID not provided' ; tput sgr0
-          exit 1;
-        fi
-    fi
-}
-
 function CreateSSHKeys() {
   # Required Argument $1 = SSH_USER
   if [ -d ~/.ssh ]
@@ -104,9 +63,6 @@ function CreateSSHKeys() {
 ## Azure Intialize           ##
 ###############################
 
-tput setaf 2; echo 'Creating Service Principal...' ; tput sgr0
-CreateServicePrincipal $PRINCIPAL_NAME
-
 tput setaf 2; echo 'Creating SSH Keys...' ; tput sgr0
 AZURE_USER=$(az account show --query user.name -otsv)
 LINUX_USER=(${AZURE_USER//@/ })
@@ -118,7 +74,6 @@ if [ -f ./params.json ]; then PARAMS="params.json"; else PARAMS="azuredeploy.par
 az deployment create --template-file azuredeploy.json  \
   --location $AZURE_LOCATION \
   --parameters $PARAMS \
-  --parameters servicePrincipalObjectId=$OBJECT_ID \
   --parameters random=$UNIQUE \
   --parameters adminUserName=$LINUX_USER
 
